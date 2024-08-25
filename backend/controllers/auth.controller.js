8b9a658a986d3c293bd85c77279f329a0cfbecb7
Zaid <zaidshaikh0024@gmail.com>
2024-08-25 19:47:06 +0530

    Feature: Implement Logout page functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -102,7 +102,7 @@ export const login = async (req,res) =>{
 }
 export const logout = async (req,res) =>{
     try {
-        res.cookie("jwt",{maxAge:0});
+        res.cookie("jwt","",{maxAge:0});
         res.status(200).json({
             message:"Logout successfully"
         })
--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,8 +5,35 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
+  const {
+    mutate: logoutMutation,
+    isError,
+    isPending,
+    error,
+  } = useMutation({
+    mutationFn: async () => {
+      try {
+        const res = await fetch("/api/auth/logout", {
+          method: "POST",
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error || "Something went wrong");
+        }
+        // navigate("/logout");
+      } catch (error) {
+        throw new Error(error);
+      }
+    },
+    onSuccess: () => {
+      toast.success("Logout successful!");
+    },
+  });
+
   const data = {
     fullName: "John Doe",
     username: "johndoe",
@@ -66,9 +93,15 @@ const Sidebar = () => {
                 </p>
                 <p className="text-slate-500 text-sm">@{data?.username}</p>
               </div>
-              <BiLogOut className="w-5 h-5 cursor-pointer" />
+              <BiLogOut
+                className="w-5 h-5 cursor-pointer"
+                onClick={(e) => {
+                  e.preventDefault();
+                  logoutMutation();
+                }}
+              />
             </div>
-          </Link> 
+          </Link>
         )}
       </div>
     </div>
