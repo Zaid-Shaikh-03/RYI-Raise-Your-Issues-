3616d304855bf5321ae5bcf795b3493cfd7f02ab
Zaid <zaidshaikh0024@gmail.com>
2024-08-26 00:18:00 +0530

    Feature: Fetch posts for the  homePage

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -22,7 +22,7 @@ const HomePage = () => {
               <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary"></div>
             )}
           </div>
-          
+
           <div
             className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
             onClick={() => setFeedType("following")}
@@ -38,7 +38,7 @@ const HomePage = () => {
         <CreatePost />
 
         {/* POSTS */}
-        <Posts />
+        <Posts feedType={feedType} />
       </div>
     </>
   );
