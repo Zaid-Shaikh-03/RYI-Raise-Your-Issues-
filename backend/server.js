845f8af1e1313f92344328800a711291a0fc0b7c
Zaid <zaidshaikh0024@gmail.com>
2024-08-26 13:41:08 +0530

    Feature: Create post function added and Payload limit updated in server.js

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,8 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(express.json())
+app.use(express.json({limit:"5mb"}))//to parse req.body
+//limit shouldn't be too high to prevent  DOS attack 
 // to parse from data(urlencoded)
 app.use(express.urlencoded({extended:true}))
 
